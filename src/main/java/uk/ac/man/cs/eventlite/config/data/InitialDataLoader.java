616a73605e1d5c90b1546ffcfb94beb5e627a9af
e65824dg <dion.geitz@student.manchester.ac.uk>
2022-03-03 15:49:51 +0000

    Seeded Event Data

--- src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
@@ -1,5 +1,8 @@
 package uk.ac.man.cs.eventlite.config.data;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
+import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
 
@@ -29,13 +32,24 @@ public class InitialDataLoader {
 			if (venueService.count() > 0) {
 				log.info("Database already populated with venues. Skipping venue initialization.");
 			} else {
-				// Build and save initial venues here.
+				
+				
+			
 			}
+			
 
 			if (eventService.count() > 0) {
 				log.info("Database already populated with events. Skipping event initialization.");
 			} else {
-				// Build and save initial events here.
+				
+				Event eA = new Event();
+				eA.setName("Event A");
+				eA.setTime(LocalTime.of(12, 0, 0));
+				eA.setDate(LocalDate.of(2022, 1, 4));
+				eventService.save(eA);
+				
+						
+				
 			}
 		};
 	}
